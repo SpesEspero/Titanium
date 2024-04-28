7247e65bace103941ec1cd2a4f89f92844269817
SpesEspero <spesespero@toloka.ai>
2024-04-28 14:25:26 +0200

    feat: add banner

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const headerBurger = document.getElementById("menu-btn");
-const header = document.querySelector(".header");
+const header = document.getElementById("header");
 
 if (headerBurger && header) {
   headerBurger.addEventListener("click", () => {
     header.classList.toggle("active");
     document.body.classList.toggle("lock");
+  });
+}
+
+const bannerCloseButton = document.getElementById("banner-btn");
+const banner = document.getElementById("banner");
+
+if (bannerCloseButton && banner) {
+  bannerCloseButton.addEventListener("click", () => {
+    banner.style.display = "none";
   });
 }
